917f8849c8eb09d5a043615ad7a2160a6c7045f9
Satyabratpro <Satyabratpro@gmail.com>
2025-05-23 22:02:20 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+num1 = (input("think of any number between 1 and 10 and type 'next': "))
+(input("then multiply it by 9 and type 'next': "))
+dig = (int(input("type how many digits: ")))
+if dig == 1:
+   print("the answer is 9")
+elif dig == 2:
+   (input("then take first digit and second digit and add them together and type 'next': "))
+   print("subtract by -1 and the answer is 8")
